33323c94ab48c45526c390740c2586d3490949b1
Daniel <daniilbondarcov@gmail.com>
2022-06-06 09:02:18 +0300

    django: added unit testing to the project.

--- dandcy-api/dandcynotes/notes/tests.py
+++ dandcy-api/dandcynotes/notes/tests.py
@@ -1,3 +1,91 @@
-from django.test import TestCase
+import django
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+django.setup()
+
+
+class AccountTests(APITestCase):
+    user = None
+    note = None
+
+    def authenticate(self, is_admin=False):
+        payload = {
+            'username': 'user',
+            'password': 'complex_passWord123'
+        }
+
+        self.client.post(reverse('register'), payload)
+
+        if is_admin:
+            payload['username'] = 'bondarkov'
+            payload['password'] = 'root'
+
+        response = self.client.post(reverse('login'), payload)
+
+        self.user = response.data['user']['id']
+
+        self.client.credentials(HTTP_AUTHORIZATION=f"Token {response.data['token']}")
+
+    def test_get_notes_unauthorized(self):
+        response = self.client.get(reverse('notes'), format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(len(response.data), 1)
+
+    def test_get_notes_authorized(self):
+        self.authenticate()
+
+        response = self.client.get(reverse('notes'), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
+
+    #def test_get_notes_admin_authorized(self):
+     #   self.authenticate(True)
+#
+ #       response = self.client.get(reverse('admin_notes'), format='json')
+  #      self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_note(self):
+        self.authenticate()
+        payload = {'content': 'test', 'title': 'stest1234kolas', 'user': self.user}
+
+        response = self.client.post(reverse('create_note'), payload, format='json')
+
+        self.note = response.data['id']
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['title'], 'stest1234kolas')
+        self.assertEqual(response.data['content'], 'test')
+
+    def test_get_note(self):
+        self.authenticate()
+
+        self.test_create_note()
+
+        response = self.client.get(reverse('get_note', args=(self.note,)), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'stest1234kolas')
+        self.assertEqual(response.data['content'], 'test')
+
+    def test_delete_note(self):
+        self.authenticate()
+
+        self.test_create_note()
+
+        response = self.client.delete(reverse('delete_note', args=(self.note,)), format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(response.data, None)
+
+    def test_update_note(self):
+        self.authenticate()
+
+        self.test_create_note()
+
+        payload = {
+            'title': 'new_title_test'
+        }
+
+        response = self.client.patch(reverse('update_note', args=(self.note,)), payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'new_title_test')
--- dandcy-api/dandcynotes/notes/urls.py
+++ dandcy-api/dandcynotes/notes/urls.py
@@ -1,19 +1,20 @@
 from django.urls import path, include, re_path
+from djoser.views import UserViewSet
 
 from .views import AdminNoteListView, NoteCreateView, NoteRetrieveView, NoteUpdateView, NoteDeleteView, \
     UserNoteListView, CustomObtainAuthToken, Logout
 
 urlpatterns = [
-    path('auth/', include('djoser.urls')),
-    path(r'auth/login/', CustomObtainAuthToken.as_view()),
+    path('auth/users/', UserViewSet.as_view({'post': 'create'}), name='register'),
+    path(r'auth/login/', CustomObtainAuthToken.as_view(), name='login'),
     path('auth/logout/', Logout.as_view()),
 
-    path('admin/notes/all/', AdminNoteListView.as_view()),
+    path('admin/notes/all/', AdminNoteListView.as_view(), name='admin_notes'),
 
-    path('notes/all/', UserNoteListView.as_view()),
-    path('notes/create/', NoteCreateView.as_view()),
+    path('notes/all/', UserNoteListView.as_view(), name='notes'),
+    path('notes/create/', NoteCreateView.as_view(), name='create_note'),
 
-    path('notes/<int:pk>/', NoteRetrieveView.as_view()),
-    path('notes/<int:pk>/update/', NoteUpdateView.as_view()),
-    path('notes/<int:pk>/delete/', NoteDeleteView.as_view()),
+    path('notes/<int:pk>/', NoteRetrieveView.as_view(), name='get_note'),
+    path('notes/<int:pk>/update/', NoteUpdateView.as_view(), name='update_note'),
+    path('notes/<int:pk>/delete/', NoteDeleteView.as_view(), name='delete_note'),
 ]
